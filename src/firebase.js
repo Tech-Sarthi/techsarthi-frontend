42e94a7eaef0a376f747683ed341c378761b9da8
unknown <bansal22kushi@gmail.com>
2022-05-09 12:31:45 +0530

    client firebase setup and express setup

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5DcneOSaSLnrJnsKa4nVkX991KumFg8c",
+  authDomain: "tech-sarthi.firebaseapp.com",
+  projectId: "tech-sarthi",
+  storageBucket: "tech-sarthi.appspot.com",
+  messagingSenderId: "488395304907",
+  appId: "1:488395304907:web:3e04bb56ef093d847e104e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth();
