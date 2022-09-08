cda9f3bb14d6376aa556516e3d05dcc11186e043
Alazord <Alazord@users.noreply.github.com>
2022-09-08 23:14:56 +0530

    Minor changes made

---
+++ src/components/profileCard/profileCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./profileCard.scss";
+
+export default function Card(props) {
+  return (
+    <div className="profile-card-box">
+      <div className="profile-card-description">
+        <div className="profile-pic">
+          <img src={props.src} alt="profile picture" />
+        </div>
+        <div className="profile-name">{props.name}</div>
+        <div className="dept-name">{props.dept}</div>
+        <div className="line"></div>
+      </div>
+      <div className="contact-details">
+        <div className="contact-details-item">
+          <img src="/logo192.png" height={20} width={20} />
+          <div className="mail">{props.mail}</div>
+        </div>
+        <div className="contact-details-item">
+          <img src="/logo192.png" height={20} width={20} />
+          <div className="phone">{props.phone}</div>
+        </div>
+        <div className="contact-details-item">
+          <img src="/logo192.png" height={20} width={20} />
+          <div className="link">{props.link}</div>
+        </div>
+        <div className="line"></div>
+      </div>
+      <button className="view-profile">View Profile</button>
+    </div>
+  );
+}
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,9 +1,82 @@
 import React from "react";
 
-function Contact(){
-    return (
-        <h1>Contact</h1>
-    );
+function Contact() {
+  const profList = [
+    {
+      name: "Prof.Amit Chodhary",
+      dept: "West Block, Mechanical and Industrial Department, IIT Roorkee",
+      mail: "amit.choudhary@me.iitr.ac.in",
+      phone: "01332-284696",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+    {
+      name: "Prof. Shushil Kumar",
+      dept: "Chemical Engineering Department, IIT Roorkee",
+      mail: "shushil@ch.iitr.ac.in",
+      phone: "01332-284944",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+    {
+      name: "Prof. Soma Rohtagi",
+      dept: "Biosciences and Bioengineering Department, IIT Roorkee",
+      mail: "soma.rohatgi@bt.iitr.ac.in",
+      phone: "01332-284831",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+    {
+      name: "Prof. Meenakshi Rawat",
+      dept: "Electronics and Communication Engineering Department, IIT Roorkee",
+      mail: "meenakshi.rawat@ece.iitr.ac.in",
+      phone: "01332-284808",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+    {
+      name: "Prof. Akshay Dvivedi",
+      dept: "West Block, Mechanical and Industrial Department, IIT Roorkee",
+      mail: "akshaydvivedi@me.iitr.ac.in",
+      phone: "01332-285428",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+    {
+      name: "Vishal",
+      dept: "SRIC Department, IIT Roorkee",
+      mail: "vishal.techsarthi@sric.iitr.ac.in",
+      phone: "01332-285378",
+      link: "www.iitr.ac.in",
+      tags: [],
+      src: "D:ProjectTechSarthi-WebsiteclientpublicprofilePic.jpg",
+    },
+  ];
+  return (
+    <div className="contact">
+      <div className="contact-header">
+        <Carousell header="TECH SARTHI" name="CONTACT US" height="400px" />
+      </div>
+      <div className="contact-list">
+        {profList.map((item, key) => (
+          <Card
+            src="/profilePic.jpg"
+            name={item.name}
+            dept={item.dept}
+            mail={item.mail}
+            phone={item.phone}
+            link={item.link}
+            tags={item.tags}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
