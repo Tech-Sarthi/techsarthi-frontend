20f082319d7061210e1a2e16d80d8b9a1f437f53
aayu1401 <aayushi_m@me.iitr.ac.in>
2023-01-18 16:30:13 +0530

    deploy

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-export const url_header = "http://localhost:8000/";
-export const register_redirect_url = "http://localhost:8000/register/complete";
+export const url_header = "http://13.233.165.1/";
+export const register_redirect_url = "http://13.233.165.1/register/complete";
